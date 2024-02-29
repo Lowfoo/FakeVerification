5338b24088657311e29ee624f30b7ef361a72c99
Lowfoo <161701340+Lowfoo@users.noreply.github.com>
2024-02-29 18:23:03 +0100

    Update index.js

--- index.js
+++ index.js
@@ -338,7 +338,7 @@ app.options('/mfa/totp', async (req, res) => {
   res.status(200).send('ok')
 })
 
-app.listen(3000, async () => {
+app.listen(8080, async () => {
   console.log('Servidor rodando na porta 80.')
 })
 
